bd2dfac32f6b69fec46a8234b0cf3bf7098fd5fe
maylove-lov <samj09808@gmail.com>
2026-07-13 15:32:23 +0700

    Override onCreate to set transparent background

--- android/app/src/main/java/com/mport/app/MainActivity.java
+++ android/app/src/main/java/com/mport/app/MainActivity.java
@@ -1,5 +1,18 @@
 package com.mport.app;
 
+import android.graphics.Color;
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            getBridge().getWebView().setBackgroundColor(Color.TRANSPARENT);
+        }
+    }
+}
